b1b7651f32d5a6b02ac0cf94651c8695f1b995cf
Keaton Burleson <keaton.burleson@fedex.com>
2021-08-30 13:57:14 -0500

    Working on new tests

--- projects/ngx-mat-facet-search/src/lib/ngx-mat-facet-search.component.spec.ts
+++ projects/ngx-mat-facet-search/src/lib/ngx-mat-facet-search.component.spec.ts
@@ -1,31 +1,30 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {NgxMatFacetSearchComponent} from './ngx-mat-facet-search.component';
-import {MAT_DIALOG_DATA, MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import {render} from '@testing-library/angular';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatIconModule} from '@angular/material/icon';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CSVPipe, FilterPipe, KeysPipe} from './pipes';
 
 describe('NgxMatFacetSearchComponent', () => {
-  let component: NgxMatFacetSearchComponent;
-  let fixture: ComponentFixture<NgxMatFacetSearchComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      providers: [
-        {provide: MatDialogRef, useValue: {}},
-        {provide: MAT_DIALOG_DATA, useValue: []},
+  test('should render component', async () => {
+    await render(NgxMatFacetSearchComponent, {
+      imports: [
+        MatAutocompleteModule,
+        MatTooltipModule,
+        MatIconModule,
+        MatChipsModule,
+        MatCheckboxModule,
+        ReactiveFormsModule,
+        FormsModule,
       ],
-      imports: [MatDialogModule, MatAutocompleteModule],
-      declarations: [NgxMatFacetSearchComponent]
-    })
-      .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(NgxMatFacetSearchComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+      declarations: [
+        CSVPipe,
+        FilterPipe,
+        KeysPipe,
+      ]
+    });
+  })
 });
